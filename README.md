f4a8f2d21061b1f75634d045aa6e12011e527278
goropikari <goropikari@users.noreply.github.com>
2017-03-07 07:37:30 +0900

    first commit

--- README.md
+++ README.md
@@ -1,7 +1,56 @@
 # LinuxNotifier
 
-[![Build Status](https://travis-ci.org/goropikari/LinuxNotifier.jl.svg?branch=master)](https://travis-ci.org/goropikari/LinuxNotifier.jl)
+This package is notification tools for Linux OS.
 
-[![Coverage Status](https://coveralls.io/repos/goropikari/LinuxNotifier.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/goropikari/LinuxNotifier.jl?branch=master)
+## Setup
+Before using LinuxNotifier package, you need to install following softwares to work correctly in your Linux system.
+- libnotify for desktop notifications
+- "mail" for e-mail notifications
 
-[![codecov.io](http://codecov.io/github/goropikari/LinuxNotifier.jl/coverage.svg?branch=master)](http://codecov.io/github/goropikari/LinuxNotifier.jl?branch=master)
+This package uses "mail" linux command to notify by e-mail. Thus you need some settings in advance.
+If following command works correctly, you don't need further setting.
+```bash
+	$ echo test | mail -s hoge yourmail@example.com
+```
+
+## Installation
+```Julia
+	Pkg.clone("git://github.com/goropikari/LinuxNotifier.jl.git")
+```
+
+## Usage
+### popup notification
+```Julia
+	using LinuxNotifier
+	lnotify("calculation done") 
+	# defalut title is set by date.
+	# You can change the title by title option, lnotify("calculation done", title="hogehoge")
+	lnotify("calculation done", sound=true) # with sound
+	lnotify("calculation done", music="./hoge.wav") # specify a sound file
+	alarm() # only sound. You can specify a sound file, alarm(music="./piyo.wav")
+```
+<img src="https://github.com/goropikari/LinuxNotifier.jl/blob/master/src/popup.png" align="left"  />
+
+
+<br><br><br><br>
+### e-mail notification
+```Julia
+	email("message", ToAddress="hoge@example.com") # defalut subject is set by date.
+	email("message", subject="result", ToAddress="hoge@example.com")
+```
+
+
+If you use "email" fn frequently, I recommend you to register your e-email address by "register_email" fn.
+```Julia
+	julia> register_email()
+	Type your desired recipient e-mail address to receive a notification.
+	e-mail: hoge@example.com
+
+	Recipient e-mail address is saved at /path/to/.julia/v0.5/LinuxNotifier/email/address.txt.
+	If you want to change the address, modify /path/to/.julia/v0.5/LinuxNotifier/email/address.txt directly or execute register_email() again
+```
+
+After you registered, you don't need specify e-mail address.
+```Julia
+	email("message")
+```
--- src/LinuxNotifier.jl
+++ src/LinuxNotifier.jl
@@ -1,5 +1,100 @@
 module LinuxNotifier
+export lnotify, register_email, email, alarm
 
-# package code goes here
+import WAV
 
+
+if is_unix() || is_linux()
+    """
+    lnotify(message; title, sound, music, time)
+
+    defalut
+
+    title = "\$(now())" \n
+    sound = false \n
+    music = "$(Pkg.dir())/LinuxNotifier/src/LinuxNotifier_sound.wav" \n
+    time = 4 # display time (seconds)
+
+    """
+    function lnotify(message::String;
+                     title="$(now())",
+                     sound=false,
+                     music="$(Pkg.dir())/LinuxNotifier/src/LinuxNotifier_sound.wav",
+                     time=4)
+        if sound || music != "$(Pkg.dir())/LinuxNotifier/src/LinuxNotifier_sound.wav"
+            run(`notify-send $title $message -i $(Pkg.dir())/LinuxNotifier/src/logo.svg -t $(time * 1000)`)
+            WAV.wavplay("$music")
+        else
+            run(`notify-send $title $message -i $(Pkg.dir())/LinuxNotifier/src/logo.svg -t $(time * 1000)`)
+        end
+    end
+
+    "notify by sound"
+    alarm(;music="$(Pkg.dir())/LinuxNotifier/src/LinuxNotifier_sound.wav") = WAV.wavplay("$music")
+
+
+    "register a recipient e-mail address"
+    function register_email()
+        output_dir = "$(Pkg.dir())/LinuxNotifier/email"
+        if ! ispath(output_dir); mkdir(output_dir); end
+
+        if ispath(output_dir * "/address.txt")
+
+            println("An e-mail address is already registered.")
+            println("Do you overwrite? [y/n]")
+            YesNo = lowercase(chomp(readline(STDIN)))
+
+            if YesNo ∈ ["n", "no"]
+                return
+            end
+
+            println("\nType your desired recipient e-mail address to receive a notification.")
+            print("e-mail: ")
+            ToAddress = chomp(readline(STDIN))
+            fp = open("$output_dir/address.txt", "w")
+            write(fp, ToAddress)
+            close(fp)
+
+        else
+            println("Type your desired recipient e-mail address to receive a notification.")
+            print("e-mail: ")
+            ToAddress = chomp(readline(STDIN))
+            fp = open("$output_dir/address.txt", "w")
+            write(fp, ToAddress)
+            close(fp)
+
+            println("\nRecipient e-mail address is saved at $output_dir/address.txt.")
+            println("If you want to change the address, modify $output_dir/address.txt directly or execute register_email() again.")
+
+        end
+
+    end
+
+
+
+    """
+    email(message; subject, ToAddress)
+
+    defalut\n
+    subject="\$(now())"\n
+    ToAddress="not-specified"\n
+
+    """
+    function email(message; subject="$(now())", ToAddress="not-specified")
+        output_dir = "$(Pkg.dir())/LinuxNotifier/email"
+        if ToAddress == "not-specified"
+            if ispath(output_dir * "/address.txt")
+                ToAddress = readline(output_dir * "/address.txt")
+            else
+                println("Email address is not specified.")
+                println("To send an e-mail, register an e-mail address by register_email() or")
+                println("specify it by ToAddress option like")
+                println("	email(\"some messages\", ToAddress=\"hoge@example.com\").")
+
+                return
+            end
+        end
+        run(pipeline(`echo $message`, `mail -s $subject $ToAddress`))
+    end
+end # if
 end # module
