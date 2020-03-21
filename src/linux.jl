a0c3b629488b5b37ed6e8f96fece1b418ba29cac
Adrian Tymorek <adrian.tymorek@protonam.com>
2020-03-21 17:04:28 +0100

    Revert "Revert "Add more backends for playing sounds in alarm and notify functions.""
    This reverts commit 6630eaf49aa015c3e058470161ad6625de515867.

--- src/linux.jl
+++ src/linux.jl
@@ -14,6 +14,7 @@ Notify by desktop notification.
 - `time::Real`: Display time.
 - `logo`: Default is Julia logo
 - `app_name::AbstractString` : Name of application sending the notification. Default is the name of your script (e.g. "test.jl") or "Julia REPL"/"Atom Juno" if using any of these two.
+- `sound_backend::AbstractString` : a CLI audio program used ("aplay","sox","vlc")
 """
 function notify(message::AbstractString;
                 title="Julia",
@@ -21,7 +22,8 @@ function notify(message::AbstractString;
                 sound::Union{Bool, AbstractString}=false,
                 time::Real=4,
                 app_name::AbstractString=PROGRAM_FILE,
-                logo::AbstractString=joinpath(@__DIR__, "logo.svg"))
+                logo::AbstractString=joinpath(@__DIR__, "logo.svg"),
+                sound_backend::AbstractString="aplay")
     if app_name == "" && occursin("REPL", @__FILE__)   # Default for running in REPL
         app_name = "Julia REPL"
     elseif occursin("atom", app_name) && occursin("boot_repl.jl", app_name)  # Default for running in Juno
@@ -29,10 +31,9 @@ function notify(message::AbstractString;
     end
     if sound == true || typeof(sound) <: AbstractString
         if sound == true
-            d = @__DIR__
-            @async run(`aplay -q $(joinpath(d, "default.wav"))`)
+            alarm(backend=sound_backend)
         elseif ispath(sound)
-            @async run(`aplay -q $sound`)
+            alarm(sound=sound, backend=sound_backend)
         end
     end
     run(`notify-send $title $message -u $urgency -a $app_name -i $(logo) -t $(time * 1000)`)
@@ -46,9 +47,19 @@ notify() = notify("Task completed.")
 
 Notify by sound.
 If you choose a specific sound WAV file, you can play it instead of the default sound.
+
+# Arguments
+- `backend::AbstractString` : a CLI audio program used ("aplay","sox","vlc")
 """
-function alarm(;sound::AbstractString=joinpath(@__DIR__, "default.wav"))
-    @async run(`aplay -q $sound`)
+function alarm(;sound::AbstractString=joinpath(@__DIR__, "default.wav"),
+                backend::AbstractString="aplay")
+    if backend == "aplay"
+        @async run(`aplay -q $sound`)
+    elseif backend == "sox"
+        @async run(`play -q $sound`)
+    elseif backend == "vlc"
+        @async run(`cvlc --play-and-exit --no-loop $sound 2> /dev/null`)
+    end
     return nothing
 end
 
